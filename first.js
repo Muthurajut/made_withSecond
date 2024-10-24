a54668c2f3d28cc7215191b252e0a9666023050b
Muthuraj <tmuthuraju98@gmail.com>
2024-10-24 15:03:57 +0530

    desgin done

---
+++ first.js
@@ -0,0 +1,13 @@
+function changecolor(){
+const textElement =document.getElementById("wprds")
+textElement.style.color="blue";
+textElement.style.fontSize="30px";
+
+
+}
+function revcolor(){
+    document.getElementById("wprds").setAttribute("style", "color:black;");
+   
+
+}
+
